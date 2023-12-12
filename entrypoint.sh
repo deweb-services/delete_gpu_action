b20c62c3c00d6dbef8d0bbd0ab599624f5ed05ca
Andrei Rybnov <rybnov@dws.sh>
2023-12-12 17:41:18 +0400

    Add escapes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-/usr/bin/deploy_script gpu delete --access_key $1 --secret_key $2 --uuid $3
+/usr/bin/deploy_script gpu delete --access_key $1 --secret_key $2 --uuid "$3"
 
 export RESULT=$(cat result)
 
